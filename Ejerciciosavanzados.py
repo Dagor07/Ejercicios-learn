ea63819989e978879739d7182e1bd567b44d3785
Dagor07 <danielgonzaortiz@gmail.com>
2025-05-14 16:51:23 -0500

    Update Ejerciciosavanzados.py

--- Ejerciciosavanzados.py
+++ Ejerciciosavanzados.py
@@ -3,39 +3,37 @@ import random
 # 🔴 Nivel Avanzado
 # Validar contraseña segura
 # Escribe una función que valide si una contraseña cumple con requisitos de seguridad (mayúsculas, minúsculas, números y símbolos).
-def es_contrasena_segura(contrasena: str):
-    mayuscula = False
-    minuscula = False
-    numero = False
-    simbolo = False
+def is_password_secure(password: str):
+    upper = False
+    lower = False
+    number = False
+    symbol = False
 
-    for caracter in contrasena:
-        if caracter.isupper():
-            mayuscula = True
-        elif caracter.islower():
-            minuscula = True
-        elif caracter.isdigit():
-            numero = True
+    for character in password:
+        if character.isupper():
+            upper = True
+        elif character.islower():
+            lower = True
+        elif character.isdigit():
+            number = True
         else:
             if not caracter.isspace():
-                simbolo = True
+                symbol = True
 
-    if mayuscula and minuscula and numero and simbolo:
+    if upper and lower and number and symbol:
         return True
     else:
         return False
-print(es_contrasena_segura("Hola1/"))
-
 
 # Simular dado
 # Crea una función que simule el lanzamiento de un dado de 6 caras.
-def lanzar_dado():
+def roll_dice():
     return random.randint(1, 6)
 
 # Suma de elementos únicos
 # Escribe una función que reciba una lista de números y devuelva la suma de los elementos únicos.
-def suma_unicos(lista):
-    return sum([x for x in lista if lista.count(x) == 1])
+def single_sum(list):
+    return sum([x for x in list if list.count(x) == 1])
 # Generador de contraseñas
 # Crea una función que genere una contraseña aleatoria de una longitud dada.
 import random
@@ -43,15 +41,20 @@ import random
 def passwordgenerator(length):
     if length < 4:
         return "La contraseña debe tener al menos 4 caracteres."
-    minusculas = "abcdefghijklmnopqrstuvwxyz"
-    mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numeros = "0123456789"
-    simbolos = "!#$%&/()=?¡+*[]}{-.,<>~|"
-    todos = minusculas + mayusculas + numeros + simbolos
-    password = ''.join(random.choice(todos) for _ in range(length))
+
+    # Definir manualmente los caracteres
+    lowers = "abcdefghijklmnopqrstuvwxyz"
+    uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "!#$%&/()=?¡+*[]}{-.,<>~|"
+
+    All = lowers + uppers + numbers + symbols
+
+    # Generar contraseña aleatoria
+    password = ''.join(random.choice(All) for _ in range(length))
     return password
 # Composición de funciones
 # Escribe una función que reciba otra función como parámetro y aplique una composición de funciones.
-def componer(func1,func2, valor):
-    return func1(func2(valor))
+def componer(func1,func2, value):
+    return func1(func2(value))
 
