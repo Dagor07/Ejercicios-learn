a5be40efc2257171176191374a607ddec8e626a9
Dagor07 <danielgonzaortiz@gmail.com>
2025-05-14 16:42:12 -0500

    Update Ejerciciosavanzados.py

--- Ejerciciosavanzados.py
+++ Ejerciciosavanzados.py
@@ -1,3 +1,5 @@
+import random
+
 # 🔴 Nivel Avanzado
 # Validar contraseña segura
 # Escribe una función que valide si una contraseña cumple con requisitos de seguridad (mayúsculas, minúsculas, números y símbolos).
@@ -22,18 +24,34 @@ def es_contrasena_segura(contrasena: str):
         return True
     else:
         return False
-print(es_contrasena_segura("holA/246 "))
+print(es_contrasena_segura("Hola1/"))
 
 
 # Simular dado
 # Crea una función que simule el lanzamiento de un dado de 6 caras.
+def lanzar_dado():
+    return random.randint(1, 6)
 
 # Suma de elementos únicos
 # Escribe una función que reciba una lista de números y devuelva la suma de los elementos únicos.
-
+def suma_unicos(lista):
+    return sum([x for x in lista if lista.count(x) == 1])
 # Generador de contraseñas
 # Crea una función que genere una contraseña aleatoria de una longitud dada.
+import random
 
+def passwordgenerator(length):
+    if length < 4:
+        return "La contraseña debe tener al menos 4 caracteres."
+    minusculas = "abcdefghijklmnopqrstuvwxyz"
+    mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numeros = "0123456789"
+    simbolos = "!#$%&/()=?¡+*[]}{-.,<>~|"
+    todos = minusculas + mayusculas + numeros + simbolos
+    password = ''.join(random.choice(todos) for _ in range(length))
+    return password
 # Composición de funciones
 # Escribe una función que reciba otra función como parámetro y aplique una composición de funciones.
+def componer(func1,func2, valor):
+    return func1(func2(valor))
 
